c45515b061a8491abdb6b4974f0bbcef017d4531
Max Zinoviev <140282696+zinoviev-tech@users.noreply.github.com>
2024-03-07 17:29:31 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
+#Postgres
+
 docker volume create postgres_vol_1
 docker volume create postgres_vol_2
 docker volume create clickhouse_vol
@@ -12,3 +14,15 @@ docker run --rm -d \
   -v postgres_vol_1:/var/lib/postgresql/data \
   --net=app_net \
   postgres:14
+
+#Superset
+
+-it superset superset fab create-admin \
+              --username admin \
+              --firstname Superset \
+              --lastname Admin \
+              --email admin@superset.com \
+              --password admin
+
+docker exec -it superset superset db upgrade
+docker exec -it superset superset load_examples
