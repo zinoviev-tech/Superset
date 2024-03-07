64033498a1203f11ca5bea72c6a1e94bbe914a2d
Max Zinoviev <140282696+zinoviev-tech@users.noreply.github.com>
2024-03-07 16:49:19 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,12 @@
+docker volume create postgres_vol_1
+docker volume create postgres_vol_2
+docker volume create clickhouse_vol
 
+docker network create app_net
+
+docker run --rm -d \
+  --name postgres_1 \
+  -e POSTGRES_PASSWORD=postgres_admin
+  -e POSTGRES_user=postgres_admin
+  -e 
+  
