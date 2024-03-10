9633e9d40e32a8c982235d6c12b8bb2abeb1f348
Max Zinoviev <140282696+zinoviev-tech@users.noreply.github.com>
2024-03-10 11:44:53 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -6,23 +6,13 @@ docker volume create clickhouse_vol
 
 docker network create app_net
 
-docker run --rm -d \
-  --name postgres_1 \
-  -e POSTGRES_PASSWORD=postgres_admin \
-  -e POSTGRES_USER=postgres_admin \
-  -e POSTGRES_DB=test_app \
-  -v postgres_vol_1:/var/lib/postgresql/data \
-  --net=app_net \
-  postgres:14
+docker run --rm -d --name postgres_1 -e POSTGRES_PASSWORD=postgres_admin -e POSTGRES_USER=postgres_admin -e POSTGRES_DB=test_app -v postgres_vol_1:/var/lib/postgresql/data --net=app_net postgres:14
 
 #Superset
 
--it superset superset fab create-admin \
-              --username admin \
-              --firstname Superset \
-              --lastname Admin \
-              --email admin@superset.com \
-              --password admin
+docker run -d --rm --net=app_net -p 80:8088 --name superset apache/superset
+
+-it superset superset fab create-admin -username admin --firstname Superset --lastname Admin --email admin@superset.com --password admin
 
 docker exec -it superset superset db upgrade
 docker exec -it superset superset load_examples
